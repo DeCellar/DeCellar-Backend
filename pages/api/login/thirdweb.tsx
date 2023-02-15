e6c845923393cf8e4ec74e2f8dafb969429e2343
Abhishek Chauhan <ac12644@gmail.com>
2023-02-15 14:14:05 +0100

    bug fix

--- pages/api/login/thirdweb.tsx
+++ pages/api/login/thirdweb.tsx
@@ -1,11 +1,15 @@
 import { ThirdwebAuth } from '@thirdweb-dev/auth/next';
-import { PrivateKeyWallet } from '@thirdweb-dev/auth/evm';
+import { SignerWallet } from '@thirdweb-dev/auth/evm';
+import { ethers } from 'ethers';
 
-const env = process.env.NODE_ENV;
+// Create a new ethers signer or use an existing one
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string);
+const wallet = new SignerWallet(signer);
+
 // Here we configure thirdweb auth with a domain and wallet
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  domain: process.env.DOMAIN || '',
-  wallet: new PrivateKeyWallet(process.env.PRIVATE_KEY || ''),
+  domain: process.env.DOMAIN as string,
+  wallet: wallet,
 });
 
 // Use the ThirdwebAuthHandler as the default export to handle all requests to /api/auth/*
