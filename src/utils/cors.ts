a1f573d18fd474c8d2a886b4032717fcad22c31c
Abhishek Chauhan <ac12644@gmail.com>
2023-02-19 01:54:56 +0100

    cors fix

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -8,8 +8,7 @@ const cors = initMiddleware(
   Cors({
     // Only allow requests with GET, POST and OPTIONS
     methods: ['GET', 'POST', 'OPTIONS'],
-    origin: DOMAIN || '*',
-    credentials: true,
+    origin: DOMAIN,
   })
 );
 
