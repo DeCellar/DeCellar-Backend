dd25837b58dc4b56f7dc67c1dcca9a9a80a174eb
Abhishek Chauhan <ac12644@gmail.com>
2023-03-29 15:11:25 +0200

    bug fix

--- src/types/evm/balance/types.ts
+++
@@ -1,9 +0,0 @@
-// Balance API Param and Result Types
-import Moralis from 'moralis';
-
-export type getNativeBalanceParams = Parameters<typeof Moralis.EvmApi.balance.getNativeBalance>[0];
-
-export type getNativeBalanceResult = Pick<
-  Awaited<ReturnType<typeof Moralis.EvmApi.balance.getNativeBalance>>,
-  'result'
->['result'];
