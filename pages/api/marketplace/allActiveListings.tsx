c3e27542183be962f9eb7f98b25ef1ecf6e7ad75
Abhishek Chauhan <ac12644@gmail.com>
2023-03-29 15:16:53 +0200

    bug fix

--- pages/api/marketplace/allActiveListings.tsx
+++ pages/api/marketplace/allActiveListings.tsx
@@ -1,11 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import cors from 'src/utils/cors';
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
-import { IListing } from 'src/types/marketplace';
 
 const { MARKETPLACE, NETWORK } = process.env;
 
-export default async function handler(req: IListing, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res);
   try {
     if (!NETWORK || !MARKETPLACE) {
