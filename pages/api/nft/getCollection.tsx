73a1c55bbe5e1b2882850231029562ba1780802d
Abhishek Chauhan <ac12644@gmail.com>
2023-05-12 11:29:05 +0200

    get collection api added

---
+++ pages/api/nft/getCollection.tsx
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import cors from 'src/utils/cors';
+import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+
+// ----------------------------------------------------------------------
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res);
+  try {
+    const { address, chainId } = req.query;
+    const sdk = new ThirdwebSDK(chainId as string);
+    const contract = await sdk.getContract(address as string, 'nft-collection');
+    console.log(contract);
+    const metadata: any = await contract.metadata.get();
+
+    return res.status(200).json({ metadata });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Internal Server Error');
+  }
+}
