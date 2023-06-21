78967dee91b726d967404bf0ee28bb1c42542196
ac12644 <ac12644@gmail.com>
2023-06-21 14:40:03 +0200

    bug fix

--- pages/api/alchemy/notify/events.tsx
+++ pages/api/alchemy/notify/events.tsx
@@ -1,9 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import cors from '../../../../src/utils/cors';
 import socketIO, { Server } from 'socket.io';
 import http from 'http';
-import axios from 'axios';
-import fetch from 'node-fetch';
+import axios from 'src/utils/axios';
+import cors from 'src/utils/cors';
 
 const PORT = process.env.PORT || 5000;
 const ALCHEMY_AUTH_KEY = process.env.ALCHEMY_AUTH_KEY;
@@ -63,7 +62,7 @@ function notificationReceived(req: NextApiRequest) {
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res);
 
-  if (req.method === 'GET' && req.url.startsWith('/api/events')) {
+  if (req.method === 'GET' && req.url?.startsWith('/api/events')) {
     const walletAddress = req.query.walletAddress as string;
 
     // Use the walletAddress to fetch the corresponding events from your data source
@@ -78,9 +77,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 async function fetchEvents(walletAddress: string) {
   try {
     // Make an API request to fetch events based on the wallet address
-    const response = await fetch(`http://localhost:3000/api/alchemy/notify/events?walletAddress=${walletAddress}`);
+    const response = await fetch(
+      `https://api.decellar.it/api/alchemy/notify/events?walletAddress=${walletAddress}`
+    );
     const events = await response.json();
-
     return events;
   } catch (error) {
     console.error('Error fetching events:', error);
