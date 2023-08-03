f07dc213fa1d647e52e72cbcdbfca6d4c144b4c5
ac12644 <ac12644@gmail.com>
2023-08-03 14:39:57 +0200

    get all time sales volume for each nft

--- pages/api/marketplace/salesVol.tsx
+++ pages/api/marketplace/salesVol.tsx
@@ -2,11 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import cors from '../../../src/utils/cors';
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 
-const { MARKETPLACE, NETWORK } = process.env;
+const { NETWORK, MARKETPLACE } = process.env;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await cors(req, res);
-
   try {
     if (!NETWORK || !MARKETPLACE) {
       return res.status(500).send('Missing required environment variables');
@@ -14,94 +13,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const sdk = new ThirdwebSDK(NETWORK);
     const contract = await sdk.getContract(MARKETPLACE, 'marketplace');
-    const getAllListings = await contract.getAllListings();
 
-    const nftsWithBid = await Promise.all(
-      getAllListings.map(async (nft) => {
-        if (nft.type === 0) {
-          // For type === 0 (non-auction listings)
-          return {
-            ...nft,
-            highestBid: null,
-            offers: [],
-          };
-        }
+    const events = await contract.events.getEvents('NewSale');
 
-        // For type === 1 (auction listings)
-        const highestBid = await contract.auction.getWinningBid(nft.id);
-        const getOffers = await contract.getOffers(nft.id);
+    // Sort sales in descending order based on the total price paid
+    const sortedSales = events.sort((a, b) => {
+      const totalPricePaidA = Number(a.data.totalPricePaid);
+      const totalPricePaidB = Number(b.data.totalPricePaid);
+      return totalPricePaidB - totalPricePaidA;
+    });
 
-        return {
-          ...nft,
-          highestBid,
-          offers: getOffers,
-        };
-      })
-    );
+    // Convert BigNumber totalPricePaid to readable format (e.g., USDT)
+    const decimalFactor = 1e6; // Assuming USDT with 6 decimal places
+    const formattedSales = [];
+    for (const sale of sortedSales) {
+      const listingId = Number(sale.data.listingId);
+      const quantityBought = Number(sale.data.quantityBought);
+      const totalPricePaid = Number(sale.data.totalPricePaid) / decimalFactor; // Convert to USDT
+      const listing = await contract.getListing(listingId.toString());
 
-    // Get the collection address from the request query parameters
-    const collectionAddress = req.query.address;
-
-    if (!collectionAddress) {
-      return res.status(400).send('Collection address parameter is missing');
+      formattedSales.push({
+        listingId,
+        assetContract: sale.data.assetContract,
+        quantityBought,
+        totalPricePaid,
+        transactionHash: sale.transaction.transactionHash,
+        blockNumber: sale.transaction.blockNumber,
+        listingDetails: listing, // Add the listing details to the response
+      });
     }
 
-    const { floorPrice, currencySymbol } = getFloorPriceAndCurrencySymbol(
-      nftsWithBid,
-      collectionAddress as string
-    );
-
-    if (floorPrice === null) {
-      return res.status(404).send('Collection not found');
-    }
-
-    return res.status(200).json({ floorPrice, currencySymbol });
+    res.status(200).json({ sales: formattedSales });
   } catch (error) {
     console.error(error);
     return res.status(500).send('Internal Server Error');
   }
 }
-
-// Function to get the floor price and currency symbol of a collection
-const getFloorPriceAndCurrencySymbol = (listings: any, collectionAddress: string) => {
-  const listingsForCollection = listings.filter(
-    (listing: any) => listing.assetContractAddress === collectionAddress
-  );
-
-  if (listingsForCollection.length === 0) {
-    // No listings found for the collection
-    return { floorPrice: null, currencySymbol: null };
-  }
-
-  // Filter out listings that have a valid buyoutCurrencyValuePerToken
-  const validDirectSaleListings = listingsForCollection.filter(
-    (listing: any) => listing.type === 0 && listing.buyoutCurrencyValuePerToken !== null
-  );
-
-  // Filter out listings that have a valid highestBid (for type === 1 - auction listings)
-  const validAuctionListings = listingsForCollection.filter(
-    (listing: any) => listing.type === 1 && listing.highestBid !== null
-  );
-
-  let floorPrice = null;
-  let currencySymbol = null;
-
-  // Calculate the floor price based on the type
-  if (validDirectSaleListings.length > 0) {
-    floorPrice = Math.min(
-      ...validDirectSaleListings.map((listing: any) =>
-        parseFloat(listing.buyoutCurrencyValuePerToken.displayValue)
-      )
-    );
-    currencySymbol = validDirectSaleListings[0].buyoutCurrencyValuePerToken.symbol;
-  } else if (validAuctionListings.length > 0) {
-    floorPrice = Math.max(
-      ...validAuctionListings.map((listing: any) =>
-        parseFloat(listing.highestBid.amount.displayValue)
-      )
-    );
-    currencySymbol = validAuctionListings[0].highestBid.amount.symbol;
-  }
-
-  return { floorPrice, currencySymbol };
-};
