595680b5c973d16288547b18ff100ec62fbd0ed0
Henry Giwa <favourgiwa3@gmail.com>
2023-09-22 17:54:12 +0100

    Added API Documentation Successfully!

---
+++ pages/api-docs.tsx
@@ -0,0 +1,27 @@
+import fs from 'fs/promises'; // Use promises-based fs
+import path from 'path';
+
+export default function ApiDocs({ html = "" }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
+
+export async function getStaticProps() {
+  try {
+    // Use path.join to construct the correct file path
+    const filePath = path.join(process.cwd(), 'public', 'swagger.html');
+    const html = await fs.readFile(filePath, 'utf8');
+
+    return {
+      props: {
+        html,
+      },
+    };
+  } catch (error) {
+    console.error("Error reading swagger.html:", error);
+    return {
+      props: {
+        html: "not working!", // Return an empty string in case of an error
+      },
+    };
+  }
+}
