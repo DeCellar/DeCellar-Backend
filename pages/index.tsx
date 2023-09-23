adcf68a0956e35e00474d54142792f7c1d920450
Abhishek <ac12644@gmail.com>
2023-09-23 12:59:50 +0200

    Removed unused dependencies & files
    📚 Index page switched to Swagger doc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,27 @@
-import Head from 'next/head';
-import { HOST_API, DOMAIN } from '../config';
+import fs from 'fs/promises'; // Use promises-based fs
+import path from 'path';
 
-export default function IndexPage() {
-  return (
-    <div style={styles.container}>
-      <Head>
-        <title>DeCellar API</title>
-        <meta name="description" content="DeCellar API for NFT marketplace" />
-        <meta name="keywords" content="wine, API, DeCellar, selling" />
-        <meta name="author" content="DeCellar" />
-        <link rel="icon" href="assets/images/logos/logo.png" />
-      </Head>
-      <img src="assets/images/logos/logo.png" style={styles.logo} alt="DeCellar Logo" />
-      <h1 style={styles.title}>Welcome to the DeCellar API</h1>
-      <p style={styles.text}>
-        You have successfully set up the DeCellar API. You can start building your backend
-        functionality by creating new API routes in the <code>pages/api</code> directory.
-      </p>
-      <p style={styles.text}>
-        The <code>HOST_API</code> variable is currently set to <code>{HOST_API}</code>. The{' '}
-        <code>DOMAIN</code> variable is currently set to <code>{DOMAIN}</code> and bockchain network
-        is set to <code>{process.env.NETWORK}</code>.
-      </p>
-    </div>
-  );
+export default function ApiDocs({ html = '' }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    background: '#ffffff',
-    fontFamily: 'Roboto, sans-serif',
-  } as React.CSSProperties,
-  logo: {
-    width: '200px',
-    height: 'auto',
-    margin: '20px 0',
-  } as React.CSSProperties,
-  title: {
-    fontSize: '48px',
-    fontWeight: 'bold',
-    color: '#4b4b4b',
-    textAlign: 'center',
-    marginBottom: '40px',
-  } as React.CSSProperties,
-  text: {
-    fontSize: '24px',
-    color: '#7f7f7f',
-    textAlign: 'center',
-    marginBottom: '20px',
-    maxWidth: '500px',
-  } as React.CSSProperties,
-};
+export async function getStaticProps() {
+  try {
+    // Use path.join to construct the correct file path
+    const filePath = path.join(process.cwd(), 'public', 'swagger.html');
+    const html = await fs.readFile(filePath, 'utf8');
+
+    return {
+      props: {
+        html,
+      },
+    };
+  } catch (error) {
+    console.error('Error reading swagger.html:', error);
+    return {
+      props: {
+        html: 'not working!', // Return an empty string in case of an error
+      },
+    };
+  }
+}
